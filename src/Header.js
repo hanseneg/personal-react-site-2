4dff5aa20aaed3e3fbfe4953283a7f2bee02ccbd
Ethan Hansen <hanseneg@hotmail.com>
2021-01-08 14:25:40 -0700

    commit1

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Header() {
+    return (
+        <div>
+            <header className="App-header">
+                <h2>Movie Posters</h2> 
+            </header>
+            <header>
+                
+            </header>
+        </div>
+    )
+}
+  
+export default Header
